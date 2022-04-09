67ec351c4e6837946d8f11532d9a3c29510d4a8d
Ruslan Abiev <abievruslan@yandex.ru>
2022-04-09 17:29:30 +0300

    add onboarding screen first

---
+++ app/src/main/java/com/rusabi/playzone/OnboardingScreenFirst.kt
@@ -0,0 +1,11 @@
+package com.rusabi.playzone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingScreenFirst : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen_first)
+    }
+}
